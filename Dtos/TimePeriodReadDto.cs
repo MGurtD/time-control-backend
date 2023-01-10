0de6ba944080f1551b8be6de13d46d3d32d50c3d
Marc Gurt <marc.gurt@mapex.io>
2023-01-10 16:56:47 +0100

    added: user identifier on timeperiod dto

--- Dtos/TimePeriodReadDto.cs
+++ Dtos/TimePeriodReadDto.cs
@@ -9,11 +9,13 @@ public class TimePeriodReadDto
         this.Id = timePeriod.Id;
         this.CreatedOn = timePeriod.CreatedOn.HasValue ? timePeriod.CreatedOn.Value : DateTime.Now;
         this.UpdatedAt = timePeriod.UpdatedAt;
+        this.UserId = timePeriod.UserId;
     }
 
     public string? Id { get; set; }
     public DateTime CreatedOn { get; set; }
     public DateTime? UpdatedAt { get; set; }
+    public string? UserId { get; set; }
 
     public string MonthId
     {
